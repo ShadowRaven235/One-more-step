f1f43e72853ede9e1307b616ebd0b274a8026a10
Frugalo W <fruga1o@hotmail.com>
2024-01-27 11:17:19 -0600

    New assets

--- Step.gd
+++ Step.gd
@@ -4,13 +4,13 @@ var direction = 1
 
 func _ready():
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, "position", position + Vector2(100 * direction,0), 2)
+	tween.tween_property(self, "position", position + Vector2(300 * direction,0), 2)
 
 func _physics_process(delta):
 	if Utils.canStep and ((direction == 1 and Input.is_action_just_pressed("ui_right")) or (direction == -1 and Input.is_action_just_pressed("ui_left"))):
 		direction = direction * -1
 		var tween = get_tree().create_tween()
-		tween.tween_property(self, "position", position + Vector2(100 * direction,0), 2)
+		tween.tween_property(self, "position", position + Vector2(300 * direction,0), 2)
 		Utils.canStep = false
 
 func _on_area_entered(area):
