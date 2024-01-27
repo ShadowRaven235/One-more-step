9a8dcdd0cc702ac1410f04dee7a7dfd7774aa034
Frugalo W <fruga1o@hotmail.com>
2024-01-27 09:32:36 -0600

    Hooking up stepping

--- Step.gd
+++ Step.gd
@@ -1,30 +1,26 @@
 extends Area2D
 
-var canStep = false
 var direction = 1
 
 func _ready():
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "position", position + Vector2(100 * direction,0), 2)
 
-func _process(delta):
-	if canStep and Input.is_action_just_pressed("ui_accept"):
+func _physics_process(delta):
+	if Utils.canStep and ((direction == 1 and Input.is_action_just_pressed("ui_right")) or (direction == -1 and Input.is_action_just_pressed("ui_left"))):
 		direction = direction * -1
 		var tween = get_tree().create_tween()
 		tween.tween_property(self, "position", position + Vector2(100 * direction,0), 2)
-		canStep = false
-		print("step")
+		Utils.canStep = false
 
 func _on_area_entered(area):
 	if area.name == "Trip":
-		print("trip")
+		Utils.tripped = true
 		queue_free()
 	if "Marker" in area.name:
-		canStep = true
-		print("step on")
+		Utils.canStep = true
 
 
 func _on_area_exited(area):
 	if "Marker" in area.name:
-		canStep = false
-		print("step off")
+		Utils.canStep = false
