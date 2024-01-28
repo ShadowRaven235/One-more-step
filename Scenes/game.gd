ae4a42ec6c1c48267ba5b5405a266e76613bea95
Frugalo W <fruga1o@hotmail.com>
2024-01-27 22:59:04 -0600

    Minor fixes

--- Scenes/game.gd
+++ Scenes/game.gd
@@ -4,10 +4,10 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("Player/Camera2D/HighScore").set_text("HighScore: " + str(Utils.highScore))
-	get_node("Player/Camera2D/TotalDistance").set_text("Total Distance: " + str(Utils.totalDistance))
+	get_node("Player/Camera2D/TotalDistance").set_text("Total Steps: " + str(Utils.totalDistance))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Utils.gameOver:
 		get_node("Player/Camera2D/HighScore").set_text("HighScore: " + str(Utils.highScore))
-		get_node("Player/Camera2D/TotalDistance").set_text("Total Distance: " + str(Utils.totalDistance))
+		get_node("Player/Camera2D/TotalDistance").set_text("Total Steps: " + str(Utils.totalDistance))
